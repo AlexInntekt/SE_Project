963d7ed1bc591905939526eff8c28d9316556161
alex <alex.inntekt@gmail.com>
2021-01-24 12:43:50 +0200

    save progress

--- project/src/database/admin.py
+++ project/src/database/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Subject, Professor, LanguageStream
+from .models import Subject, Professor, LanguageStream, Faculty, Department
 
 admin.site.register(Subject)
 admin.site.register(Professor)
 admin.site.register(LanguageStream)
+admin.site.register(Faculty)
+admin.site.register(Department)
--- project/src/database/models.py
+++ project/src/database/models.py
@@ -17,11 +17,34 @@ class Subject(models.Model):
 	def __str__(self):
 		return "{} {}".format(self.name, self.language.abrv)
 
+class Faculty(models.Model):
+	name = models.CharField(max_length=100)
+	abrv = models.CharField(max_length=50, null=True)
+
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		verbose_name_plural = 'Faculties'
+
+class Department(models.Model):
+	name = models.CharField(max_length=50)
+	faculty = models.ForeignKey(Faculty, null=True, on_delete=models.SET_NULL)
+
+	def __str__(self):
+		return self.name
+
 
 class Professor(models.Model):
 	name = models.CharField(max_length=50)
 	surname = models.CharField(max_length=50)
 	subject = models.ManyToManyField(Subject)
+	department = models.ForeignKey(Department, null=True, on_delete=models.SET_NULL)
 
 	def __str__(self):
-		return self.name
+		try:
+			department_name = self.department.name
+		except AttributeError:
+			department_name = "No department assigned"
+			
+		return "{} {} ({})".format(self.name, self.surname, department_name)
