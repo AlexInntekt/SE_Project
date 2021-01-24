fa75ffdf986edcac2910e1b3ac7a5b7fe5dd94f5
alex <alex.inntekt@gmail.com>
2021-01-24 17:46:31 +0200

    save progress

--- project/src/database/models.py
+++ project/src/database/models.py
@@ -4,47 +4,64 @@ from django.db import models
 
 
 class LanguageStream(models.Model):
-	name = models.CharField(max_length=50)
-	abrv = models.CharField(max_length=3, null=True)
+    name = models.CharField(max_length=50)
+    abrv = models.CharField(max_length=3, null=True)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
 class Subject(models.Model):
-	name = models.CharField(max_length=50)
-	language = models.ForeignKey(LanguageStream, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=50)
+    language = models.ForeignKey(LanguageStream, on_delete=models.SET_NULL, null=True)
 
-	def __str__(self):
-		return "{} {}".format(self.name, self.language.abrv)
+    def __str__(self):
+        return "{} {}".format(self.name, self.language.abrv)
 
 class Faculty(models.Model):
-	name = models.CharField(max_length=100)
-	abrv = models.CharField(max_length=50, null=True)
+    name = models.CharField(max_length=100)
+    abrv = models.CharField(max_length=50, null=True)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
-	class Meta:
-		verbose_name_plural = 'Faculties'
+    class Meta:
+        verbose_name_plural = 'Faculties'
 
 class Department(models.Model):
-	name = models.CharField(max_length=50)
-	faculty = models.ForeignKey(Faculty, null=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=50)
+    faculty = models.ForeignKey(Faculty, null=True, on_delete=models.SET_NULL)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
 
 class Professor(models.Model):
-	name = models.CharField(max_length=50)
-	surname = models.CharField(max_length=50)
-	subject = models.ManyToManyField(Subject)
-	department = models.ForeignKey(Department, null=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    subject = models.ManyToManyField(Subject)
+    department = models.ForeignKey(Department, null=True, on_delete=models.SET_NULL)
 
-	def __str__(self):
-		try:
-			department_name = self.department.name
-		except AttributeError:
-			department_name = "No department assigned"
+    def __str__(self):
+        try:
+            department_name = self.department.name
+        except AttributeError:
+            department_name = "No department assigned"
 
-		return "{} {} ({})".format(self.name, self.surname, department_name)
+        return "{} {} ({})".format(self.name, self.surname, department_name)
+
+
+class TeachingHour(models.Model):
+    DAY = [
+        ('Monday','Monday'),
+        ('Tuesday','Tuesday'),
+        ('Wednesday','Wednesday'),
+        ('Thursday','Thursday'),
+        ('Friday','Friday'),
+        ('Saturday','Saturday'),
+        ('Sunday','Sunday')
+    ]
+
+    professor = models.ForeignKey(Professor, null=True, on_delete=models.SET_NULL)
+    subject   = models.ForeignKey(Subject, null=True, on_delete=models.SET_NULL)
+    day       = models.CharField(null=True, max_length=20, choices=DAY, default='Monday')
+    hours      = models.IntegerField(null=True, default=2)
