8a052ef40a1333bde5a143cd62376c4813efa833
alex <alex.inntekt@gmail.com>
2021-01-24 19:36:06 +0200

    save progress

--- project/src/api/urls.py
+++ project/src/api/urls.py
@@ -3,5 +3,17 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-path('v1/languages', LanguageStreamView.as_view())
+path('v1/languages', LanguageStreamView.as_view()),
+path('v1/subjects', SubjectView.as_view()),
+path('v1/faculties', FacultyView.as_view()),
+path('v1/departments', DepartmentView.as_view()),
+path('v1/professors', ProfessorView.as_view()),
+path('v1/teaching_hours', TeachingHourView.as_view()),
+
+path('v1/add_language', AddLanguageStreamView.as_view()),
+path('v1/add_subject', AddSubjectView.as_view()),
+path('v1/add_faculty', AddFacultyView.as_view()),
+path('v1/add_department', AddDepartmentView.as_view()),
+path('v1/add_professor', AddProfessorView.as_view()),
+path('v1/add_teaching_hour', AddTeachingHourView.as_view()),
 ]
--- project/src/api/views.py
+++ project/src/api/views.py
@@ -20,3 +20,121 @@ class LanguageStreamView(generics.ListAPIView):
         self.response_list = self.serializer_class(queryset, many=True, context={'request':request}).data
 
         return Response(self.response_list, status=status.HTTP_200_OK)
+
+
+
+class SubjectView(generics.ListAPIView):
+
+    response_list = []
+    serializer_class = SubjectSerializer
+    queryset = Subject.objects.all()
+
+    def list(self, request, *args, **kwargs):
+
+        queryset = Subject.objects.all()
+        
+        self.response_list = self.serializer_class(queryset, many=True, context={'request':request}).data
+
+        return Response(self.response_list, status=status.HTTP_200_OK)
+
+
+
+class FacultyView(generics.ListAPIView):
+
+    response_list = []
+    serializer_class = FacultySerializer
+    queryset = Faculty.objects.all()
+
+    def list(self, request, *args, **kwargs):
+
+        queryset = Faculty.objects.all()
+        
+        self.response_list = self.serializer_class(queryset, many=True, context={'request':request}).data
+
+        return Response(self.response_list, status=status.HTTP_200_OK)
+
+
+
+
+class DepartmentView(generics.ListAPIView):
+
+    response_list = []
+    serializer_class = DepartmentSerializer
+    queryset = Department.objects.all()
+
+    def list(self, request, *args, **kwargs):
+
+        queryset = Department.objects.all()
+        
+        self.response_list = self.serializer_class(queryset, many=True, context={'request':request}).data
+
+        return Response(self.response_list, status=status.HTTP_200_OK)
+
+
+
+class ProfessorView(generics.ListAPIView):
+
+    response_list = []
+    serializer_class = ProfessorSerializer
+    queryset = Professor.objects.all()
+
+    def list(self, request, *args, **kwargs):
+
+        queryset = Professor.objects.all()
+        
+        self.response_list = self.serializer_class(queryset, many=True, context={'request':request}).data
+
+        return Response(self.response_list, status=status.HTTP_200_OK)
+
+
+class TeachingHourView(generics.ListAPIView):
+
+    response_list = []
+    serializer_class = TeachingHourSerializer
+    queryset = TeachingHour.objects.all()
+
+    def list(self, request, *args, **kwargs):
+
+        queryset = TeachingHour.objects.all()
+        
+        self.response_list = self.serializer_class(queryset, many=True, context={'request':request}).data
+
+        return Response(self.response_list, status=status.HTTP_200_OK)
+
+
+
+
+
+
+
+
+
+
+class AddLanguageStreamView(generics.CreateAPIView):
+
+	serializer_class = LanguageStreamSerializer
+
+
+class AddSubjectView(generics.CreateAPIView):
+
+	serializer_class = SubjectSerializer
+
+
+class AddFacultyView(generics.CreateAPIView):
+
+	serializer_class = FacultySerializer
+
+
+class AddDepartmentView(generics.CreateAPIView):
+
+	serializer_class = DepartmentSerializer
+
+
+class AddProfessorView(generics.CreateAPIView):
+
+	serializer_class = ProfessorSerializer
+
+
+class AddTeachingHourView(generics.CreateAPIView):
+
+	serializer_class = TeachingHourSerializer
